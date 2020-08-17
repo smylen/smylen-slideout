b69b6b45ca7ce5de02dc46af412bc4cb306b727f
Juan M. Villegas <juan.villgs@gmail.com>
2020-08-17 11:15:47 +0200

    v1

---
+++ dev/serve.vue
@@ -0,0 +1,17 @@
+<script>
+import Vue from 'vue';
+import SmylenSlideout from '@/SmylenSlideout.vue';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  components: {
+    SmylenSlideout
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <SmylenSlideout />
+  </div>
+</template>
---
+++ src/SmylenSlideout.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="SmylenSlideout">
+    <div class="slideout-backdrop" @click.prevent="onBackdropClick" v-if="showSlideout"></div>
+    <div class="slideout" v-if="showSlideout">
+      <a href="#" @click.prevent="onCloseClick" class="slideout__closeBtn">Close</a>
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SmylenSlideout',
+  props: {
+    showSlideout: {
+      type: Boolean,
+      default: false,
+      required: false,
+    },
+    onClose: {
+      type: Function,
+      default: () => {},
+      required: false,
+    }
+  },
+  data() {
+    return {
+    };
+  },
+  computed: {
+
+  },
+  methods: {
+    onBackdropClick() {
+      this.onClose();
+    },
+    onCloseClick() {
+      this.onClose();
+    }
+  },
+};
+</script>
+
+<style scoped>
+  .SmylenSlideout {
+    display: block;
+    width: 400px;
+    margin: 25px auto;
+    border: 1px solid #ccc;
+    background: #eaeaea;
+    text-align: center;
+    padding: 25px;
+  }
+  .SmylenSlideout p {
+    margin: 0 0 1em;
+  }
+</style>
